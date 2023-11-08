fbc1697166bffc26db344c59db592d1b4f83e694
Ty Mull <sabertoothchicken@gmail.com>
2023-11-07 17:45:35 -0700

    page setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import './App.css';
+import ContentPage from './components/ContentPage';  // ensure you import the ContentPage component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<ContentPage page="Home" />} />
+          <Route path="/campus-events" element={<ContentPage page="Campus Events" />} />
+          <Route path="/community-events" element={<ContentPage page="Community Events" />} />
+          <Route path="/campus-facilities" element={<ContentPage page="Campus Facilities" />} />
+          <Route path="/community-facilities" element={<ContentPage page="Community Facilities" />} />
+          <Route path="/activities" element={<ContentPage page="Activities" />} />
+          <Route path="/food" element={<ContentPage page="Food" />} />
+          <Route path="/clubs" element={<ContentPage page="Clubs" />} />
+          <Route path="/organized-sports" element={<ContentPage page="Organized Sports" />} />
+          <Route path="/equipment" element={<ContentPage page="Equipment" />} />
+          <Route path="/parks" element={<ContentPage page="Parks" />} />
+          
+          {/* Add more routes for other dropdown items... */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
